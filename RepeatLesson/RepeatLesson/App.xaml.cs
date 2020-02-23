0344ac30e01d65f31e610b464a1947a90521cfe9
mustafahicyilmaz35 <mustafahicyilmaz35@gmail.com>
2020-02-23 12:48:44 +0300

    Birinci Commit

---
+++ RepeatLesson/RepeatLesson/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using RepeatLesson.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace RepeatLesson
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new MyMainPage());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ RepeatLesson/RepeatLesson/ViewModels/MyMainPageViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows.Input;
+using RepeatLesson.Views;
+using Xamarin.Forms;
+
+namespace RepeatLesson.ViewModels
+{
+    class MyMainPageViewModel
+    {
+        //private ICommand _layoutCommand;
+
+        public ICommand LayoutCommand
+        {
+            get => new Command(GoToLayoutsPageAsync);
+        }
+
+        private async void GoToLayoutsPageAsync()
+        {
+            await Application.Current.MainPage.Navigation.PushAsync(new LayoutsPage());
+        }
+
+        public MyMainPageViewModel()
+        {
+            
+        }
+    }
+}
